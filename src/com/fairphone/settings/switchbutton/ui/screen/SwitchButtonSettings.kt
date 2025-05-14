92386c880e579ed090c662913dbfb0d02ced1499
Eduardo Pinar Menoyo <eduardo@fairphone.com>
2025-05-14 21:13:03 +0200

    Remove switch settings header image

--- src/com/fairphone/settings/switchbutton/ui/screen/SwitchButtonSettings.kt
+++ src/com/fairphone/settings/switchbutton/ui/screen/SwitchButtonSettings.kt
@@ -17,15 +17,12 @@
 
 package com.fairphone.settings.switchbutton.ui.screen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -46,9 +43,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -148,16 +143,6 @@ fun SwitchButtonSettingsScreen(
             style = prefScreenHeaderTextStyle,
             modifier = Modifier
                 .padding(horizontal = 8.dp, vertical = 8.dp),
-        )
-
-        Image(
-            painter = painterResource(R.drawable.switch_button_settings_header),
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1.38f / 1f)
-                .clip(RoundedCornerShape(20.dp))
-                .height(320.dp)
         )
 
         if (!enabled) {
